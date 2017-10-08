7e16ee8a0323a3ca98a25c540017882ed0514358
WINDOWS-MT88Q05\User <hydro.ea@list.ru>
2017-10-09 00:51:30 +0400

    Added search functional

---
+++ app/src/main/java/com/gnoemes/simplepath/MainView.java
@@ -0,0 +1,22 @@
+package com.gnoemes.simplepath;
+
+import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.SkipStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public interface MainView extends MvpView {
+
+    @StateStrategyType(SkipStrategy.class)
+    void showLocation(LatLng latLng);
+    @StateStrategyType(SkipStrategy.class)
+    void setDirection(PolylineOptions direction, LatLngBounds bounds);
+    @StateStrategyType(SkipStrategy.class)
+    void setMarkers(MarkerOptions... markerOptions);
+    @StateStrategyType(SkipStrategy.class)
+    void updateLastConfiguration(PolylineOptions direction, LatLngBounds bounds, MarkerOptions... markerOptions);
+
+}
--- app/src/main/java/com/gnoemes/simplepath/MapsActivity.java
+++ app/src/main/java/com/gnoemes/simplepath/MapsActivity.java
@@ -1,50 +1,46 @@
 package com.gnoemes.simplepath;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 
-import com.akexorcist.googledirection.DirectionCallback;
-import com.akexorcist.googledirection.GoogleDirection;
-import com.akexorcist.googledirection.constant.TransportMode;
-import com.akexorcist.googledirection.model.Direction;
-import com.akexorcist.googledirection.util.DirectionConverter;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.ArrayList;
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, DirectionCallback {
 
     private GoogleMap mMap;
-    private final String API_KEY = getResources().getString(R.string.google_maps_key);
     private LatLng from;
     private LatLng to;
     private LatLng myLocation;
+    private boolean showMyLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         Intent intent = getIntent();
-        from = intent.getExtras().getParcelable("from");
-        to = intent.getExtras().getParcelable("to");
-        requestDirection();
+        showMyLocation = intent.getBooleanExtra("myLocation",false);
+        if (!showMyLocation) {
+            from = intent.getExtras().getParcelable("from");
+            to = intent.getExtras().getParcelable("to");
+        }
     }
 
     /**
@@ -60,6 +56,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setOnMyLocationChangeListener(myLocationChangeListener);
+
         // Add a marker in Sydney and move the camera
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -73,17 +70,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         mMap.setMyLocationEnabled(true);
         mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        if (showMyLocation) {
+            Location myLocation = getMyLocation();
+            if (myLocation != null) {
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(myLocation.getLatitude(), myLocation.getLongitude()), 17));
+//            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mMap.getMyLocation().getLatitude(),mMap.getMyLocation().getLongitude()),17));
+            }
+        }
         Log.i("DEVE", "onMapReady: ");
     }
 
 
-    private void requestDirection() {
-        GoogleDirection.withServerKey(API_KEY)
-                .from(from)
-                .to(to)
-                .transitMode(TransportMode.DRIVING)
-                .execute(this);
-    }
+
 
     private GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
         @Override
@@ -93,25 +91,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     };
 
 
-    @Override
-    public void onDirectionSuccess(Direction direction, String rawBody) {
-        if (direction.isOK()) {
-            mMap.addMarker(new MarkerOptions().position(from));
-            mMap.addMarker(new MarkerOptions().position(to));
+//    @Override
+//    public void onDirectionSuccess(Direction direction, String rawBody) {
+//        if (direction.isOK()) {
+//            mMap.addMarker(new MarkerOptions().position(from));
+//            mMap.addMarker(new MarkerOptions().position(to));
+//
+//            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+//            builder.include(from);
+//            builder.include(to);
+//            builder.include(myLocation);
+//            LatLngBounds bounds = builder.build();
+//            mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds,100));
+//            ArrayList<LatLng> points = direction.getRouteList().get(0).getLegList().get(0).getDirectionPoint();
+//            mMap.addPolyline(DirectionConverter.createPolyline(this, points,5, Color.BLACK));
+//        }
+//    }
+//
+//    @Override
+//    public void onDirectionFailure(Throwable t) {
+//        t.printStackTrace();
+//    }
 
-            LatLngBounds.Builder builder = new LatLngBounds.Builder();
-            builder.include(from);
-            builder.include(to);
-            builder.include(myLocation);
-            LatLngBounds bounds = builder.build();
-            mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds,100));
-            ArrayList<LatLng> points = direction.getRouteList().get(0).getLegList().get(0).getDirectionPoint();
-            mMap.addPolyline(DirectionConverter.createPolyline(this, points,5, Color.BLACK));
+    private Location getMyLocation() {
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return null;
         }
-    }
-
-    @Override
-    public void onDirectionFailure(Throwable t) {
-        t.printStackTrace();
+        Location myLocation = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (myLocation == null) {
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+            String provider = lm.getBestProvider(criteria, true);
+            myLocation = lm.getLastKnownLocation(provider);
+        }
+        return myLocation;
     }
 }
